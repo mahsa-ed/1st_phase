3f329ef44db778ff51201efc4d94d7d3e1bf0022
mahsa-ed <mahsaedalati8@gmail.com>
2024-12-04 19:19:39 +0330

    Add files via upload

--- main.c
+++ main.c
@@ -8,26 +8,33 @@ int main(void) {
     int size;
     int Vcount, Xcount,Ccount;
     int i, j, k;
+    printf("please enter the map size");
     scanf("%d", &size);
     int goldRate[size][size],foodRate[size][size],valu[size][size];
     for (i = 0; i < size; i++){
         for (j = 0; j < size; j++){
             Map[i][j] = '1';}}
 
-    printf("please ")
+    printf("please enter the number of castles");
     scanf("%d",&Ccount); //قرار گیری قلمرو ها
-    for(k=0;k<Ccount;k++){
+    for(k=1;k<=Ccount;k++){
+        printf("enter the coordinates of castle %d",k);
         scanf("%d %d",&i, &j);
         Map[i-1][j-1]='C';}
 
+    printf("please enter the number of villages");
     scanf("%d",&Vcount); //قرار گیری روستا ها و نرخ تولید
-    for(k=0;k<Vcount; k++){
+    for(k=1;k<=Vcount; k++){
+        printf("enter the coordinates of village %d",k);
         scanf("%d %d",&i, &j);
         Map[i-1][j-1]='V';
+        printf("enter the gold rate and food rate of village %d",k);
         scanf("%d %d",&goldRate[i-1][j-1], &foodRate[i-1][j-1]);}
 
-    scanf("%d",&Xcount); //قرار گیری خانه های مسدود
-    for(k=0;k<Xcount;k++){
+    printf("please enter the number of blocked houses");
+    scanf("%d",&Xcount);//قرار گیری خانه های مسدود
+    for(k=1;k<=Xcount;k++){
+        printf("enter the coordinates of house %d",k);
         scanf("%d %d",&i, &j);
         Map[i-1][j-1]='X';}
 
