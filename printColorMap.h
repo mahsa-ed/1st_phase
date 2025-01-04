8779eb81a3084782d4346d05a925997195effdce
mahsa-ed <mahsaedalati8@gmail.com>
2025-01-05 00:14:37 +0330

    Add files via upload

---
+++ printColorMap.h
@@ -0,0 +1,34 @@
+//
+// Created by mahsa on 1/4/2025.
+//
+
+#ifndef PRINTCOLORMAP_H
+#define PRINTCOLORMAP_H
+
+#endif //PRINTCOLORMAP_H
+#include <stdio.h>
+#include <windows.h>
+#include <conio.h>
+#include "my_struct.h"
+void setTextColor(int textColor, int backColor) {
+    HANDLE consoleHandle = GetStdHandle(STD_OUTPUT_HANDLE);
+    int colorAttribute = backColor << 4 | textColor;
+    SetConsoleTextAttribute(consoleHandle, colorAttribute);
+}
+
+void colorMap(char arr[][MAXSIZE],int size) {
+    int i,j;
+    for(i=0;i<size;i++) {
+        for(j=0;j<size;j++) {
+            if (arr[i][j]=='C') setTextColor(1,0);
+            else if (arr[i][j]=='X') setTextColor(4,0);
+            else if (arr[i][j]=='R') setTextColor(2,0);
+            else if(arr[i][j]=='V') setTextColor(5,0);
+            else if (arr[i][j]=='v') setTextColor(3,0);
+            else if (arr[i][j]=='1' || arr[i][j]=='2' || arr[i][j]=='3' || arr[i][j]=='4') setTextColor(6,0);
+            printf("%c ",arr[i][j]);
+        }
+        printf("\n");
+    }
+    setTextColor(7,0);
+}
