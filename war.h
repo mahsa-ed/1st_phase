10424a57c4e78a3fd40084a3d1aeb7a319f046cb
mahsa-ed <mahsaedalati8@gmail.com>
2025-01-25 15:29:53 +0330

    Add files via upload

--- war.h
+++ war.h
@@ -5,4 +5,49 @@
 #ifndef WAR_H
 #define WAR_H
 
+#include <stdio.h>
+#include "my_struct.h"
+
+void simpleWar(){}
+
+void allOutWar(){}
+
+int checkWar(char Map[][MAXSIZE],int x, int y,char yourSign){
+    char enemyR, enemyC;
+    int i;
+    if(yourSign == 'R') {
+        enemyC= 'c';
+        enemyR= 'r';
+    }
+    else {
+        enemyC='C';
+        enemyR='R';
+    }
+    int rowDir[] = {1, -1, 0, 0};
+    int colDir[] = {0, 0, 1, -1};
+    for(i=0;i<4;i++) {
+        if (Map[x+rowDir[i]][y+colDir[i]]==enemyC) {
+            return -1;// -1= all out war
+        }
+    }
+    for(i=0;i<4;i++) {
+        if(Map[x+rowDir[i]][y+colDir[i]]==enemyR) {
+            return 0; // 0= simple war
+        }
+    }
+    for(i=0;i<4;i++) {
+        if(Map[x+rowDir[i]][y+colDir[i]]=='v'){
+            int newX=x+rowDir[i];
+            int newY=y+colDir[i];
+            for(int j=0;j<4;j++) {
+                if(Map[newX+rowDir[j]][newY+colDir[j]]==enemyR) {
+                    return 0; // 0= simple war
+                }
+            }
+        }
+    }
+    return 1;// 1= no war
+}
+
+
 #endif //WAR_H
