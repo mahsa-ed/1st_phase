e857484f80feea875666ebb5ef2e5a03fb6b4736
mahsa-ed <mahsaedalati8@gmail.com>
2025-01-25 18:13:38 +0330

    Add files via upload

--- printColorMap.h
+++ printColorMap.h
@@ -6,6 +6,7 @@
 #define PRINTCOLORMAP_H
 
 #endif //PRINTCOLORMAP_H
+
 #include <stdio.h>
 #include <windows.h>
 #include <conio.h>
@@ -20,10 +21,10 @@ void colorMap(char arr[][MAXSIZE],int size) {
     int i,j;
     for(i=0;i<size;i++) {
         for(j=0;j<size;j++) {
-            if (arr[i][j]=='C') setTextColor(1,0);
+            if (arr[i][j]=='C' || arr[i][j]=='R') setTextColor(1,0);
+            else if(arr[i][j]=='c' || arr[i][j]=='r') setTextColor(5,0);
             else if (arr[i][j]=='X') setTextColor(4,0);
-            else if (arr[i][j]=='R') setTextColor(2,0);
-            else if(arr[i][j]=='V') setTextColor(5,0);
+            else if(arr[i][j]=='V') setTextColor(2,0);
             else if (arr[i][j]=='v') setTextColor(3,0);
             else if (arr[i][j]=='1' || arr[i][j]=='2' || arr[i][j]=='3' || arr[i][j]=='4') setTextColor(6,0);
             printf("%c ",arr[i][j]);
