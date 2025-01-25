dbf5bc375f58051e97fa8ed1eece25e7110c8a37
mahsa-ed <mahsaedalati8@gmail.com>
2025-01-25 18:12:42 +0330

    Add files via upload

--- war.h
+++ war.h
@@ -8,11 +8,54 @@
 #include <stdio.h>
 #include "my_struct.h"
 
-void destroyRoad(){}
+void destroyRoad(int x, int y, int size, char sign, char Map[][MAXSIZE], char map1[][MAXSIZE],char map2[][MAXSIZE]) {
+    int i,j;
+    if(map1[x][y]==sign) {
+        map1[x][y]=Map[x][y];
+        map2[x][y]=Map[x][y];
+    }
+    for(i=x+1,j=y; i<size && map1[i][j]==sign; i++) {
+        map1[i][j]=Map[i][j];
+        map2[i][j]=Map[i][j];
+    }
+    for(i=x-1,j=y; i>=0 && map1[i][j]==sign; i--) {
+        map1[i][j]=Map[i][j];
+        map2[i][j]=Map[i][j];
+    }
+    for(i=x, j=y+1; j<size && map1[i][j]==sign; j++) {
+        map1[i][j]=Map[i][j];
+        map2[i][j]=Map[i][j];
+    }
+    for(i=x, j=y-1; j>=0 && map1[i][j]==sign; j--) {
+        map1[i][j]=Map[i][j];
+        map2[i][j]=Map[i][j];
+    }
+}
 
-void simpleWar(){}
+int simpleWar(Ruler *yourWlt,Ruler *enemyWlt) {
+    if (yourWlt->soldiers > enemyWlt->soldiers) {
+        printf("You win the war!\n");
+        yourWlt->food+=enemyWlt->food;
+        yourWlt->gold+=enemyWlt->gold;
+        enemyWlt->food=0;
+        enemyWlt->gold=0;
+        return 1;
+    }
+    if (yourWlt->soldiers == enemyWlt->soldiers) {
+        printf("Equal war!\n");
+        return 0;
+    }
+    if(yourWlt->soldiers < enemyWlt->soldiers){
+        printf("You lose the war!\n");
+        enemyWlt->food+=yourWlt->food;
+        enemyWlt->gold+=yourWlt->gold;
+        yourWlt->food=0;
+        yourWlt->gold=0;
+        return -1;
+    }
+}
 
-int checkWar(char Map[][MAXSIZE],int x, int y,char yourSign){
+int checkWar(char map[][MAXSIZE],int x, int y,char yourSign,int Array[]){
     char enemyR, enemyC;
     int i;
     if(yourSign == 'R') {
@@ -26,21 +69,25 @@ int checkWar(char Map[][MAXSIZE],int x, int y,char yourSign){
     int rowDir[] = {1, -1, 0, 0};
     int colDir[] = {0, 0, 1, -1};
     for(i=0;i<4;i++) {
-        if (Map[x+rowDir[i]][y+colDir[i]]==enemyC) {
+        if (map[x+rowDir[i]][y+colDir[i]]==enemyC) {
             return -1;// -1= all out war
         }
     }
     for(i=0;i<4;i++) {
-        if(Map[x+rowDir[i]][y+colDir[i]]==enemyR) {
+        if(map[x+rowDir[i]][y+colDir[i]]==enemyR) {
+            Array[0]=x;
+            Array[1]=y;
             return 0; // 0= simple war
         }
     }
     for(i=0;i<4;i++) {
-        if(Map[x+rowDir[i]][y+colDir[i]]=='v'){
+        if(map[x+rowDir[i]][y+colDir[i]]=='v'){
             int newX=x+rowDir[i];
             int newY=y+colDir[i];
             for(int j=0;j<4;j++) {
-                if(Map[newX+rowDir[j]][newY+colDir[j]]==enemyR) {
+                if(map[newX+rowDir[j]][newY+colDir[j]]==enemyR) {
+                    Array[0]=newX;
+                    Array[1]=newY;
                     return 0; // 0= simple war
                 }
             }
